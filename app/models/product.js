92560108615e44246d6855551ca33b9fdfa847ca
2509roblox <2509roblox@gmail.com>
2025-01-15 18:38:01 +0700

    first commit

---
+++ app/models/product.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const ProductSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  promotion: {
+    type: Number,
+    default: 0
+  },
+  quantity: {
+    type: Number,
+    default: 0
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  slide: {
+    type: [String]
+  },
+  color: {
+    type: [String]
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true
+  },
+  beats: {
+    type: String
+  },
+  caseSize: {
+    type: String
+  },
+  caseThickness: {
+    type: String
+  },
+  movement: {
+    type: String
+  },
+  powerReserve: {
+    type: String
+  },
+  waterResistance: {
+    type: String
+  },
+  winding: {
+    type: String
+  }
+}, { timestamps: true }, { collection: 'product' });
+
+ProductSchema.plugin(mongoosePaginate);
+module.exports = mongoose.model('Product', ProductSchema);
